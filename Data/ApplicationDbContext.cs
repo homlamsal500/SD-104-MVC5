15e703ae1799af3d1fcc3a7e02cc70d8734d8d8b
Hom Lamsal <hom.geek005@gmail.com>
2019-09-05 14:03:31 -0500

    c

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcWebApplicationMucisStore.Models;
+
+namespace MvcWebApplicationMucisStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MvcWebApplicationMucisStore.Models.Music> Music { get; set; }
+        public DbSet<MvcWebApplicationMucisStore.Models.Songs> Songs { get; set; }
+    }
+}
